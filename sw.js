02fee4434c73f4f97a597af4314a915b1fb5a69f
Mássimo García <massimo.garcia85@gmail.com>
2026-09-20 16:37:25 -0600

    V624

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'gestor-cgs-v623';
-const FILES = ['./index.html', './manifest.json', './js/sheets-sync.js', './GP_activ.png', './GP_spalsh.png', './favicon.png'];
+const CACHE = 'gestor-cgs-v624';
+const FILES = ['./index.html', './manifest.json', './js/drive-sync.js', './js/sheets-sync.js', './GP_activ.png', './GP_spalsh.png', './favicon.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
